9c9ed4466fa0b171a39bf3fc6cd2906e871569bb
Pedro Henrique <phmartins.developer@gmail.com>
2023-03-06 11:15:16 -0300

    estudos

---
+++ 14-API-no-backend/main.js
@@ -0,0 +1,11 @@
+const url = "http://localhost:5500/api"
+
+function getUsers() {
+    axios.get(url)
+    .then(response => {
+        apiResult.textContent = JSON.stringify(response.data)
+    })
+    .catch(err => console.log(err));
+}
+
+getUsers();
