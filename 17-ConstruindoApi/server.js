6e5d8953012805594dd0e2a22217d78c22d863aa
Pedro Henrique <phmartins.developer@gmail.com>
2023-03-10 17:04:41 -0300

    estudos

--- 17-ConstruindoApi/server.js
+++ 17-ConstruindoApi/server.js
@@ -8,14 +8,7 @@ app.get("/clients", (req, res) => {
   res.json(data);
 });
 
-app.get("/clients/:id", (req, res) => {
-  const { id } = req.params;
-  const client = data.find(cli => cli.id == id);
 
-  if(!client) return res.status(204).json();
-
-  res.json(client);
-});
 
 app.listen(3000, () => {
   console.log("Server is running");
